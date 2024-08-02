e9021b858f2aff3090951aff5fd823c94802463d
Sourabh Kumar <sourabh.47512@gmail.com>
2024-08-02 17:24:07 +0530

    added delivery profile screen

--- app/src/main/java/com/sourabh/eggbucket/delivery_profile.kt
+++ app/src/main/java/com/sourabh/eggbucket/delivery_profile.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class delivery_profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
