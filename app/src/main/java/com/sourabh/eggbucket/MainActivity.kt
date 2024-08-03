e707bf36ba3b52abe68216626ce6f98ed452b139
Sourabh Kumar <sourabh.47512@gmail.com>
2024-08-03 11:54:57 +0530

    updated UI

--- app/src/main/java/com/sourabh/eggbucket/MainActivity.kt
+++ app/src/main/java/com/sourabh/eggbucket/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_delivery_profile)
+        setContentView(R.layout.activity_mode_of_payment)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
